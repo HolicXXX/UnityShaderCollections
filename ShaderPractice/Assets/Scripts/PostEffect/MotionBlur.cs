4e0d27fc883f5aaed79af093b3803d387df2201f
Allen <allendk@foxmail.com>
2018-02-08 11:07:05 +0800

    MotionBlur

---
+++ ShaderPractice/Assets/Scripts/PostEffect/MotionBlur.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShaderPractice {
+	public class MotionBlur : PostEffectsBase {
+		public Shader motionBlurShader;
+		private Material motionBlurMaterial = null;
+		public Material material {
+			get {
+				motionBlurMaterial = CheckShaderAndCreateMaterial (motionBlurShader, motionBlurMaterial);
+				return motionBlurMaterial;
+			}
+		}
+
+		[Range(0f, 0.9f)]
+		public float blurAmount = 0.7f;
+
+		private RenderTexture accumulationTexture;
+
+		void OnRenderImage (RenderTexture src, RenderTexture dest) {
+			if (material != null) {
+				if (accumulationTexture == null || accumulationTexture.width != src.width || accumulationTexture.height != src.height) {
+					DestroyImmediate (accumulationTexture);
+					accumulationTexture = new RenderTexture (src.width, src.height, 0);
+					accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+					Graphics.Blit (src, accumulationTexture);
+				}
+
+				//we are accumulating motion over frames without clear/discard
+				//by design, so silence any performance warnings from Unity
+				accumulationTexture.MarkRestoreExpected ();
+
+				material.SetFloat ("_BlurAmount", 1f - blurAmount);
+
+				Graphics.Blit (src, accumulationTexture, material);
+				Graphics.Blit (accumulationTexture, dest);
+			} else {
+				Graphics.Blit (src, dest);
+			}
+		}
+
+		void OnDisable () {
+			DestroyImmediate (accumulationTexture);
+		}
+	}
+}
---
+++ ShaderPractice/Assets/Shaders/PostEffect/MotionBlur.shader
@@ -0,0 +1,68 @@
+﻿Shader "Custom/MotionBlur" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_BlurAmount ("Blur Amount", Float) = 1.0
+	}
+	SubShader {
+		CGINCLUDE
+
+		sampler2D _MainTex;
+		fixed _BlurAmount;
+
+		#include "UnityCG.cginc"
+
+		struct v2f {
+			float4 pos : SV_POSITION;
+			half2 uv : TEXCOORD0;
+		};
+
+		v2f vert(appdata_img v) {
+			v2f o;
+
+			o.pos = UnityObjectToClipPos(v.vertex);
+
+			o.uv = v.texcoord;
+
+			return o;
+		}
+
+		fixed4 fragRGB(v2f i) :SV_TARGET {
+			return fixed4(tex2D(_MainTex, i.uv).rgb, _BlurAmount);
+		}
+
+		half4 fragAlpha(v2f i) : SV_TARGET {
+			return tex2D(_MainTex, i.uv);
+		}
+
+		ENDCG
+
+		ZTest Always Cull Off ZWrite Off
+
+		pass {
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			ColorMask RGB
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment fragRGB
+
+			ENDCG
+		}
+
+		pass {
+			Blend One Zero
+
+			ColorMask A
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment fragAlpha
+
+			ENDCG
+		}
+	}
+	FallBack Off
+}
